30fc7e9f1ab8adfa62ee5468b95391eb7a436ea9
lazar <lazar.srb.v@gmail.com>
2024-01-07 22:16:02 +0100

    Implement threading in the script

--- emailing.py
+++ emailing.py
@@ -12,6 +12,7 @@ RECEIVER = os.getenv('USERNAME_MY')
 
 
 def send_email(image_path):
+    print('send_email function started')
     email_message = EmailMessage()
     email_message['Subject'] = 'New customer showed up!'
     email_message.set_content('Hey, we just saw a new customer!')
@@ -27,6 +28,7 @@ def send_email(image_path):
     gmail.login(SENDER, PASSWORD)
     gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
     gmail.quit()
+    print('send_email function ended')
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import glob
 import os
 from emailing import send_email
+from threading import Thread
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
@@ -12,10 +13,14 @@ status_list = []
 
 count = 1
 
+
 def clean_folder():
+    print('clean_folder function started')
     images = glob.glob('images/*.png')
     for image in images:
         os.remove(image)
+    print('clean_folder function ended')
+
 
 while True:
     status = 0
@@ -52,8 +57,12 @@ while True:
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email(image_with_object)
-        clean_folder()
+        email_thread = Thread(target=send_email, args=(image_with_object,))
+        email_thread.daemon = True
+        clean_thread = Thread(target=clean_folder)
+        clean_thread.daemon = True
+
+        email_thread.start()
 
     print(status_list)
 
@@ -63,7 +72,8 @@ while True:
     if key == ord('q'):
         break
 
-video.release()
+clean_thread.start()
 
+video.release()
 
 
